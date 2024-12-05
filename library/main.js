d77f7cf6d47ecc4419792ddd9e7637732f1fe6a3
Scott Johnston <137368865+scottpjsco@users.noreply.github.com>
2024-12-05 17:46:14 +0900

    Added icons to the book cards

--- library/main.js
+++ library/main.js
@@ -44,6 +44,25 @@ function createReadElement(bookItem, book) {
     return read;
 }
 
+function createEditIcon(book){
+    return createBookElement('div', null, "");
+}
+
+function createIcons(){
+    const div = createBookElement('div' , null, 'icons');
+    const icon1 = document.createElement('img');
+    icon1.src = '../icons/star-plus-outline.svg';
+    const icon2 = document.createElement('img');
+    icon2.src = '../icons/eye-plus-outline.svg';
+    const icon3 = document.createElement('img');
+    icon3.src = '../icons/source-branch.svg';
+
+    div.appendChild(icon1);
+    div.appendChild(icon2);
+    div.appendChild(icon3);
+    return div;
+}
+
 function createBookItem (book,index){
     const bookItem = document.createElement ('div');
     bookItem.setAttribute('id', index)
@@ -59,6 +78,9 @@ function createBookItem (book,index){
         createBookElement('h1', `Pages: ${book.pages}`, "book-pages")
     );
     bookItem.appendChild(createReadElement(bookItem,book))
+    bookItem.appendChild(createBookElement('button', 'X', 'delete'))
+    bookItem.appendChild(createIcons());
+    bookItem.appendChild(createEditIcon(book));
 
     books.insertAdjacentElement("afterbegin", bookItem);
 }
